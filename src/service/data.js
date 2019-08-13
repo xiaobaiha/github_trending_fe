dfb7a345e797b8fc1d39f3e16bd415349a3513c9
feizhijun <feizhijun@bytedance.com>
2019-08-13 16:52:37 +0800

    fix: cors

--- src/service/data.js
+++ src/service/data.js
@@ -3,16 +3,16 @@ const API = `http://104.168.234.244:5017/`;
 export const post = data => {
   return fetch(API + 'trending', {
     body: JSON.stringify(data), // must match 'Content-Type' header
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, same-origin, *omit
+    // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    // credentials: 'same-origin', // include, same-origin, *omit
     headers: {
       'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'text/plain'
+      'Content-Type': 'application/json'
     },
     method: 'POST', // *GET, POST, PUT, DELETE, etc.
     mode: 'cors', // no-cors, cors, *same-origin
-    redirect: 'follow', // manual, *follow, error
-    referrer: 'no-referrer', // *client, no-referrer
+    // redirect: 'follow', // manual, *follow, error
+    // referrer: 'no-referrer', // *client, no-referrer
   })
     .then(response => response.json())
 }
